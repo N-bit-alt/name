7412500577741601bfb6fc95e77f60cc875b5cec
Natalia <ivanoffqa@gmail.com>
2021-01-26 13:27:42 -0600

    my second commit

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
 res.send('Hello World');
@@ -9,4 +15,12 @@ app.get('/bye', (req, res) => {
     res.send('Bye Bye');
     } );
 
+app.post('/', (a, b) => {
+    console.log(JSON.stringify(a.body))
+    b.send('Post');
+});
+
+
+
 app.listen(3000, () => console.log('Listening on port 3000...'));
+
